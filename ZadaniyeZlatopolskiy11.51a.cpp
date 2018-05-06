35125746b0f1f454da0dbe8ea85c7555190a201d
Katyapuchkova1 <Katyapuchkova1@gmail.com>
2018-05-06 23:40:46 +0300

    Update ZadaniyeZlatopolskiy11.51a.cpp

--- ZadaniyeZlatopolskiy11.51a.cpp
+++ ZadaniyeZlatopolskiy11.51a.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 int main()
 {
-	int b, a1, quontity, number;
-	cout << "Write quontity of the elements" << endl;
-	cin >> quontity;
+	int b, a1, number;
+	int const quontity = 5;
 	int mas [quontity];
 	cout << "Write b"<< endl;
 	cin >> b;
-	cout << "Write a1, be attentiv it must less then quontity " << endl;
+	cout << "Write a1, be attentive it must less then quontity " << endl;
 	cin >> a1;
 	int mas[quontity];
 	for (int i = 0; i <= quontity - 1; i++)
